b72ba7651ffb8af996338dd00697ccb98287f131
James van Leuven <jamesvl@beckermayer.com>
2015-11-04 11:46:29 -0800

    uploading guessing game with question 5

--- day-three/guessinggame/game.js
+++ day-three/guessinggame/game.js
@@ -27,9 +27,31 @@ question3 = question3.toLowerCase();
 console.log("Response 3: " + question3);
 
 
-if (question3 == "water" || question3 == "water.") {
+if (question3 == "Wine" || question3 == "wine") {
 	alert("Yum, me too!  " + question3 + " is the best!");
 	} else {
-	alert("That\'s a nice drink.");
+	alert("That\'s a nice drink too.");
 }
 
+var question4 = prompt("Do I eat meat?");
+question4 = question4.toLowerCase();
+console.log("Response 4: " + question4);
+
+
+if (question4 == "No" || question4 === "no" || question4 === "n" || question4 === "nope" || question4 === "nah") {
+	alert("That is correct, I am pescatarian.");
+	} else {
+	alert("Good for you.");
+}
+
+
+var question5 = prompt("Do you believe in God?");
+question5 = question5.toLowerCase();
+console.log("Response 5: " + question5);
+
+
+if (question5 == "No" || question5 === "no" || question5 === "n" || question5 === "nope" || question5 === "nah") {
+	alert("That is correct, I don't believe in God.");
+	} else {
+	alert("Do you believe in Santa Claus as well?");
+}
